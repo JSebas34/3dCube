61f4bb4f26804e40e8ff5c64e0cfe5ab29102bde
ihjuan3 <jugopeis2001@gmail.com>
2025-02-13 14:40:44 -0800

    update structure (not finished)

--- include/functions.cpp
+++ include/functions.cpp
@@ -20,3 +20,26 @@ string loadShader(const string& filePath) {
 
     return filebuffer.str();
 }
+
+void SetupBuffers(GLuint &VBO, GLuint &VAO, GLint posAttrib) {
+    float vertices[] = {
+            0.0f, 0.5f,
+            0.5f, -0.5f,
+            -0.5, -0.5f
+    };
+
+    glGenBuffers(1, &VBO);      //generate buffer
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);     //activate/bind buffer to target
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);      //put data into the buffer
+
+    glEnableVertexAttribArray(posAttrib);
+
+    glVertexAttribPointer(posAttrib,2,GL_FLOAT,GL_FALSE,0,(void*)0);    //specifies how to interpret vbd when drawing calls
+
+    glBindBuffer(GL_ARRAY_BUFFER,0); //unbinds
+    glBindVertexArray(0); //unbinds
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
-#include <glad.h>
+#ifdef _WIN32
+    #include "glad_Windows_NT/glad.h"
+#elif __APPLE__
+    #include "glad_Darwin/glad.h"
+#else
+    #error "Unsupported platform"
+#endif
 #include "SFML/Graphics.hpp"
 #include <SFML/OpenGL.hpp>
 #include <string>
@@ -17,18 +23,7 @@ int main()
     window.setActive(true);
 
     bool running = true;
-
-    GLuint vbo; //unsingned int
-    GLuint VAO;
-
-    glGenBuffers(1, &vbo);      //generate buffer
-
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);     //activate/bind buffer to target
-
-
+    
     while (running) {
         sf::Event event;
 
@@ -40,13 +35,6 @@ int main()
                 break;
             }
         }
-    
-    
-    float vertices[] = {
-            0.0f, 0.5f,
-            0.5f, -0.5f,
-            -0.5, -0.5f
-    };
 
 
     string vertexCode = loadShader("Vshader.glsl");
@@ -75,14 +63,12 @@ int main()
 
     GLint posAttrib = glGetAttribLocation(shaderProgram, "position");   //pos of atributtes
 
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);      //put data into the buffer
-
-    glVertexAttribPointer(posAttrib,2,GL_FLOAT,GL_FALSE,0,(void*)0);    //specifies how to interpret vbd when drawing calls
     glEnableVertexAttribArray(posAttrib); //enables attributes
 
     glUseProgram(shaderProgram);        //use program
     glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES,0, sizeof(vertices));
-    return 0;
+
 }
+return 0;
 }
